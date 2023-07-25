dbd3ba06cba86b43591041b66513779b6c7f3c33
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-07-25 21:55:36 +0100

    feat: setup basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/root.jsx
@@ -0,0 +1,9 @@
+export default function Root() {
+  return (
+    <>
+      <h1 className="text-5xl text-brand font-body font-bold">
+        Hello, React Spotify :D
+      </h1>
+    </>
+  );
+}
