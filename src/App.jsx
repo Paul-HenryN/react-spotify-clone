be1286b08c77ce4e29fc4483fa2886982981882c
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-07-25 21:35:02 +0100

    feat: setup theme variables

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-5xl">Vite + React</h1>
+      <h1 className="text-5xl text-brand font-body font-bold">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: "CircularSp, CircularSp-Arab, CircularSp-Hebr, CircularSp-Cyrl, CircularSp-Grek, CircularSp-Deva, Helvetica Neue, helvetica, arial, Hiragino Kaku Gothic Pro, Meiryo, MS Gothic",
+    },
+    extend: {
+      colors: {
+        brand: "#1ed760",
+        "accent-1": "#ae2997",
+        "accent-2": "#519af4",
+      },
+    },
   },
   plugins: [],
 };
