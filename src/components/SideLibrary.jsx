0fc2206b84f789fafc1cf954496ef2bb8ccb9d39
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-08-01 21:00:51 +0100

    refactor: extract SideLibrary component

---
+++ src/components/SideLibrary.jsx
@@ -0,0 +1,76 @@
+import SectionContainer from "./SectionContainer";
+import RoundedButton from "./Buttons/RoundedButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faListDots, faPlus, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import CircularButton from "./Buttons/CircularButton";
+import VerticalCard from "./Cards/VerticalCard";
+import SecondaryButton from "./Buttons/SecondaryButton";
+import BorderedButton from "./Buttons/BorderedButton";
+
+export default function SideLibrary() {
+  return (
+    <SectionContainer className="h-full justify-stretch items-stretch">
+      <div className="relative flex justify-between items-center px-7 py-4 shadow-lg shadow-neutral-950">
+        <RoundedButton className="flex gap-3 items-center text-neutral-400 hover:text-white transition-colors duration-300">
+          <FontAwesomeIcon icon={faListDots} />
+          Your Library
+        </RoundedButton>
+
+        <CircularButton className="p-2 hover:bg-neutral-800 text-neutral-400 hover:text-white transition-colors">
+          <FontAwesomeIcon icon={faPlus} />
+        </CircularButton>
+      </div>
+
+      <div className="flex flex-col gap-6 h-40 pb-10 overflow-auto px-2">
+        <VerticalCard className="gap-3 items-start bg-neutral-800 p-4 rounded-lg">
+          <p className="font-bold">Create your first playlist</p>
+          <p className="text-sm font-bold">It&apos;s easy, we will help you</p>
+          <SecondaryButton className="px-5 py-2">
+            Create playlist
+          </SecondaryButton>
+        </VerticalCard>
+
+        <VerticalCard className="gap-3 items-start bg-neutral-800 p-4 rounded-lg">
+          <p className="font-bold">Let&apos;s find some podcasts to follow</p>
+          <p className="text-sm font-bold">
+            We&apos;ll keep you updated on new episodes
+          </p>
+          <SecondaryButton className="px-5 py-2">
+            Browse podcasts
+          </SecondaryButton>
+        </VerticalCard>
+      </div>
+
+      <div className="flex flex-col gap-10 items-start px-6 justify-between py-5">
+        <ul className="flex flex-wrap text-[0.7rem] text-neutral-400 gap-3">
+          <li>
+            <a href="#">Legal</a>
+          </li>
+          <li>
+            <a href="#">Privacy Center</a>
+          </li>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Cookies</a>
+          </li>
+          <li>
+            <a href="#">About Ads</a>
+          </li>
+          <li>
+            <a href="#">Accessibilty</a>
+          </li>
+          <li>
+            <a href="#">Cookies</a>
+          </li>
+        </ul>
+
+        <BorderedButton className="hover:scale-105 px-4 py-1 flex items-center gap-1 text-sm font-bold">
+          <FontAwesomeIcon icon={faGlobe} size="md" />
+          English
+        </BorderedButton>
+      </div>
+    </SectionContainer>
+  );
+}
