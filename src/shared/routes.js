74ac48fc03c283fb79bcf42ea492d6ddb269a62a
paul-henryN <ngpaulhenry@gmail.com>
2024-01-18 12:05:19 +0100

    fix: links

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -3,7 +3,7 @@ const base = "/react-spotify-clone";
 export const routes = {
   HOME: base,
   LOGIN: `${base}/login`,
-  SEARCH: `${base}/search`,
+  SEARCH: `${base}/`,
   PLAYLIST: `${base}/playlist`,
 };
 
