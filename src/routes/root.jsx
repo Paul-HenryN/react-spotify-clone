acd4b283f35084542769cc5df1b0804d56515e89
paul-henryN <ngpaulhenry@gmail.com>
2024-01-18 12:16:59 +0100

    style: chrome scrollbars

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -10,7 +10,7 @@ export default function Root() {
     <div className="h-screen min-w-[50rem] grid overflow-hidden grid-cols-[min-content_auto] gap-y-2 p-2 bg-black">
       <Sidebar />
 
-      <SectionContainer className="overflow-scroll relative bg-local bg-gradient-to-b from-neutral-800 via-neutral-900 to-neutral-900">
+      <SectionContainer className="overflow-auto relative bg-local bg-gradient-to-b from-neutral-800 via-neutral-900 to-neutral-900">
         <header className="sticky top-0 px-7 py-5 bg-[hsla(0,0%,7%,0.7)] mb-[-4rem] z-50">
           <Navbar />
         </header>
